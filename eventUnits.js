d1d8bb8cbe6534b27f59b8f8b6f38a9098865e6a
zhuwen1 <1140523662@qq.com>
2016-06-03 05:07:42 +0800

    添加了对一些常用事件的获取
    入事件类型，事件目标。默认事件阻止和阻止事件冒泡等行为

--- eventUnits.js
+++ eventUnits.js
@@ -12,6 +12,7 @@ var eventUnit ={
 			element['on'+type] = handler;
 		}
 	},
+	//删除句柄
 	removeHandler:function(element,type,handler){
 		if(element.removeEventListener){
 			element.removeEventListener(type,handler,false);
@@ -20,6 +21,33 @@ var eventUnit ={
 
 		}else{
 			element['on'+type] = null;
+		}
+	},
+	//获取事件类型
+	getType:function(event){
+		return event.type;
+	}
+	getElement:function(event){
+		return event.target||event.srcElement;
+	}
+	//获取事件
+	getEvent:function(event){
+		return event?event:window.event;
+	},
+	//阻止事件默认行为
+	preventDefault:function(event){
+		if(event.preventDefault){
+			event.preventDefault();
+		}else{
+			event.returnValue();
+		}
+	},
+	//阻止事件冒泡
+	stopPropagation:function(event){
+		if(event.stopPropagation){
+			event.stopPropagation();
+		}else{
+			event.cancelBubble=true;//兼容ie
 		}
 	}
 }
